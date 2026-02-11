54221d4243f238f84fbf8fbccf0c2a819c1f8860
Leonardo Pereda Chánez <leonardopereda7@hotmail.com>
2026-02-10 20:29:59 -0600

    Variables entorno netlify

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-// ✅ TU URL REAL (Extraída de tu conexión anterior)
-const supabaseUrl = 'https://qhictrmgiulnsfaqxtog.supabase.co';
-// ✅ TU KEY REAL (Extraída de tu conexión anterior)
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoaWN0cm1naXVsbnNmYXF4dG9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NTQ4NjIsImV4cCI6MjA4NjMzMDg2Mn0.Sxet-l9vbFYVCUGY-PvDcXyQETtwqvMHLX51x0DsKy4';
+// Esto permite que use las variables de Netlify en producción 
+// y tu .env en local automáticamente.
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
